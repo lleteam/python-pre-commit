e1f716f2ca30ff8bdf5b6fb55a574252a6943dae
Long Le <longle@LL45Q2N3PH.local>
2024-07-19 16:06:13 +1000

    add env

--- src/mock_sum.py
+++ src/mock_sum.py
@@ -1,2 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def mock_sum(a, b):
+    print(f"SECRET: {os.getenv('SECRET')}")
+    print(f"NO SECRET: {os.getenv('NO_SECRET')}")
     return a + b + 1
